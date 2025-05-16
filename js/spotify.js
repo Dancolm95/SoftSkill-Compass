78688c10c5e504c5be34a5b2c2d2fd439c829c85
Daniel Colona <dancolm95@gmail.com>
2025-05-16 17:32:45 -0500

    fix bug with token spotify

--- js/spotify.js
+++ js/spotify.js
@@ -1,16 +1,16 @@
-const clientId = '78efcd81223145fa91347ff0ea67de4e';
-const clientSecret = '5571aaeff47541ff85fbab6edfdf4ca8';
+const clientId = "78efcd81223145fa91347ff0ea67de4e";
+const clientSecret = "5571aaeff47541ff85fbab6edfdf4ca8";
 
 export async function obtenerTokenSpotify() {
   const base64 = btoa(`${clientId}:${clientSecret}`);
 
-  const resp = await fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
+  const resp = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
     headers: {
-      'Authorization': `Basic ${base64}`,
-      'Content-Type': 'application/x-www-form-urlencoded'
+      Authorization: `Basic ${base64}`,
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: 'grant_type=client_credentials'
+    body: "grant_type=client_credentials",
   });
 
   const data = await resp.json();
@@ -18,25 +18,36 @@ export async function obtenerTokenSpotify() {
 }
 
 export async function buscarPlaylist(emocion, token) {
-  const resp = await fetch(`https://api.spotify.com/v1/search?q=${emocion}&type=playlist&limit=20`, {
-    headers: {
-      'Authorization': `Bearer ${token}`
+  const resp = await fetch(
+    `https://api.spotify.com/v1/search?q=${emocion}&type=playlist&limit=20`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     }
-  });
+  );
 
-  const data = await resp.json();
+  /*   const data = await resp.json();
   const playlists = data.playlists.items;
 
   if (playlists.length === 0) {
     throw new Error ('No se encuentra ninguna playlist');
+  } */
+  const data = await resp.json();
+
+  if (!data.playlists || !Array.isArray(data.playlists.items) || data.playlists.items.length === 0) {
+    throw new Error(`No se encontró ninguna playlist para: ${emocion}`);
   }
 
+  const playlists = data.playlists.items;
   const randomIndex = Math.floor(Math.random() * playlists.length);
-  return playlists[randomIndex]
+  return playlists[randomIndex];
 }
 
 async function obtenerFrase() {
-  const response = await fetch('https://api.quotable.io/random?tags=inspirational');
+  const response = await fetch(
+    "https://api.quotable.io/random?tags=inspirational"
+  );
   const data = response.json();
   return data;
 }
