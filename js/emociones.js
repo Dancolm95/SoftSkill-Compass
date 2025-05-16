ba9d1fd539af277e9dc46484419a60512d2ae2bd
Daniel Colona <dancolm95@gmail.com>
2025-05-15 22:21:38 -0500

    change the structure

--- js/emociones.js
+++ js/emociones.js
@@ -1,8 +1,24 @@
-const emociones = {
-  feliz: 'happy',
-  triste: 'sad',
-  molesto: 'angry',
-  ansioso: 'anxious'
-};
+const emociones = [
+  {
+    nombre: "Feliz",
+    frase: "¡Qué alegría! 😊",
+    imagen: "feliz.jpeg"
+  },
+  {
+    nombre: "Triste",
+    frase: "Estoy contigo 💙",
+    imagen: "triste.jpeg"
+  },
+  {
+    nombre: "Ansioso",
+    frase: "Respira, todo está bien ⏳",
+    imagen: "ansioso.jpeg"
+  },
+  {
+    nombre: "Molesto",
+    frase: "Respira hondo, todo pasa 🧘",
+    imagen: "molesto.jpeg"
+  }
+];
 
-export default emociones
+export default emociones;
